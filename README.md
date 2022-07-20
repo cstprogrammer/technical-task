f16852f5e304bac04b8314c0f8dba07605339f44
charlest <charles.thakuri@shangrilagroup.com.np>
2022-07-20 17:24:47 +0545

    UserProfile Controller file added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # technical-task
+
+# Getting started
+
+Clone the repository
+
+   https://github.com/cstprogrammer/technical-task
+
+
+----------
+
+# Code overview
+
+## File
+
+- UserProfileController.php
---
+++ UserProfileController.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profile;
+use App\Models\User;
+use File;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Image;
+use Validator;
+use View;
+
+class UserProfileController extends Controller
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Fetch user
+     * (You can extract this to repository method).
+     *
+     * @param $username
+     * @return mixed
+     */
+    public function getUserByUsername($username)
+    {
+        return User::with('profile')->wherename($username)->firstOrFail();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  string  $username
+     * @return Response
+     */
+    public function show($username)
+    {
+        try {
+            $user = $this->getUserByUsername($username);
+        } catch (ModelNotFoundException $exception) {
+            abort(404);
+        }
+
+
+        return view('profiles.show',compact('user'));
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function updateUserAccount(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $emailCheck = ($request->input('email') !== '') && ($request->input('email') !== $user->email);
+        $rules = [];
+
+        if ($user->name !== $request->input('name')) {
+            $usernameRules = [
+                'name' => 'required|max:255|unique:users',
+            ];
+        } else {
+            $usernameRules = [
+                'name' => 'required|max:255',
+            ];
+        }
+        if ($emailCheck) {
+            $emailRules = [
+                'email' => 'email|max:255|unique:users',
+            ];
+        } else {
+            $emailRules = [
+                'email' => 'email|max:255',
+            ];
+        }
+        $additionalRules = [
+            'first_name' => 'nullable|string|max:255',
+            'last_name'  => 'nullable|string|max:255',
+        ];
+
+        $rules = array_merge($usernameRules, $emailRules, $additionalRules);
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+
+        $user->name = strip_tags($request->input('name'));
+        $user->first_name = strip_tags($request->input('first_name'));
+        $user->last_name = strip_tags($request->input('last_name'));
+
+        if ($emailCheck) {
+            $user->email = $request->input('email');
+        }
+
+        $user->save();
+
+        return redirect('profile/'.$user->name.'/edit')->with('success', trans('profile.updateAccountSuccess'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateUserPasswordRequest  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function updateUserPassword(UpdateUserPasswordRequest $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        if ($request->input('password') !== null) {
+            $user->password = Hash::make($request->input('password'));
+        }
+        $user->save();
+
+        return back();
+    }
+
+    /**
+     * Upload and Update user avatar.
+     *
+     * @param $file
+     * @return mixed
+     */
+    public function upload(Request $request)
+    {
+        if ($request->hasFile('file')) {
+            $currentUser = \Auth::user();
+            $avatar = $request->file('file');
+            $filename = 'avatar.'.$avatar->getClientOriginalExtension();
+            $save_path = storage_path().'/users/id/'.$currentUser->id.'/uploads/images/avatar/';
+            $path = $save_path.$filename;
+            $public_path = '/images/profile/'.$currentUser->id.'/avatar/'.$filename;
+
+            // Make the user a folder and set permissions
+            File::makeDirectory($save_path, $mode = 0755, true, true);
+
+            // Save the file to the server
+            Image::make($avatar)->resize(300, 300)->save($save_path.$filename);
+
+            // Save the public image path
+            $currentUser->profile->avatar = $public_path;
+            $currentUser->profile->save();
+
+            return response()->json(['path' => $path], 200);
+        } else {
+            return response()->json(false, 200);
+        }
+    }
+
+    /**
+     * Show user avatar.
+     *
+     * @param $id
+     * @param $image
+     * @return string
+     */
+    public function userProfileAvatar($id, $image)
+    {
+        return Image::make(storage_path().'/users/id/'.$id.'/uploads/images/avatar/'.$image)->response();
+    }
+}
